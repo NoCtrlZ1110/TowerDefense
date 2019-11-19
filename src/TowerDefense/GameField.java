77cd12544325aa008ce81453a265915d5a1caac9
tunc2112 <sueruan012@gmail.com>
2019-11-19 16:28:25 +0700

    Update GameField.java

--- src/TowerDefense/GameField.java
+++ src/TowerDefense/GameField.java
@@ -192,8 +192,6 @@ public class GameField {
                 });
             else
                 towers.forEach(Tower::removeRange);
-        
-            location = null; // giải phóng bộ nhớ
         });
 
         layout.setOnMouseClicked(event -> {
